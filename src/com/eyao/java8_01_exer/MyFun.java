26341302dc57464e39fe46214a307f18ddcd44e2
eyao <2294553196@qq.com>
2019-02-28 12:25:26 +0800

    Lambda 表达式语法练习01

---
+++ src/com/eyao/java8_01_exer/MyFun.java
@@ -0,0 +1,12 @@
+package com.eyao.java8_01_exer;
+
+/**
+ * @Auther: lss
+ * @Date: 2019/2/28 12:13
+ * @Description:
+ */
+@FunctionalInterface
+public interface MyFun {
+
+    public String getValue(String str);
+}
---
+++ src/com/eyao/java8_01_exer/TestLambda.java
@@ -0,0 +1,54 @@
+package com.eyao.java8_01_exer;
+
+import com.eyao.java8_01.Employee;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Auther: lss
+ * @Date: 2019/2/28 12:07
+ * @Description:
+ */
+public class TestLambda {
+
+    List<Employee> employees = Arrays.asList(
+            new Employee("张三", 18, 9999.99),
+            new Employee("李四", 38, 7777.99),
+            new Employee("王五", 38, 8888.88),
+            new Employee("赵六", 58, 5555.99),
+            new Employee("田七", 8, 3333.99)
+    );
+
+    @Test
+    public void test1() {
+        Collections.sort(employees, (e1, e2) -> {
+            if (e1.getAge() == e2.getAge())
+                return e1.getName().compareTo(e2.getName());
+            else
+                return Integer.compare(e1.getAge(), e2.getAge());
+        });
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    @Test
+    public void test2() {
+        String s = strHandler("\t\t\t 你很厉害哦", (str) -> str.trim());
+        System.out.println(s);
+
+        String s1 = strHandler("abcdE", (str) -> str.toUpperCase());
+        System.out.println(s1);
+    }
+
+    // 需求：用于处理字符串
+    public String strHandler(String str, MyFun mf) {
+        return mf.getValue(str);
+    }
+
+
+}
