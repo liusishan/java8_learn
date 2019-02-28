7c48607fd7bc774c49dc07d6d488cb45ac47cdac
eyao <2294553196@qq.com>
2019-02-28 12:06:00 +0800

    Lambda 表达式语法实战

---
+++ src/com/eyao/java8_01/MyFun.java
@@ -0,0 +1,11 @@
+package com.eyao.java8_01;
+
+/**
+ * @Auther: lss
+ * @Date: 2019/2/28 12:02
+ * @Description:
+ */
+@FunctionalInterface
+public interface MyFun<T> {
+    public Integer getValue(Integer num);
+}
--- src/com/eyao/java8_01/TestLambda2.java
+++ src/com/eyao/java8_01/TestLambda2.java
@@ -104,7 +104,13 @@ public class TestLambda2 {
     // 需求：对一个数进行运算
     @Test
     public void test6() {
+        Integer num = operation(100, (x) -> x * x);
+        System.out.println(num);
 
+        System.out.println(operation(200, (y) -> y + 200));
     }
 
+    public Integer operation(Integer num, MyFun<Integer> mf) {
+        return mf.getValue(num);
+    }
 }
