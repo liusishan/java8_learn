39c4f4cc49762d3046dfe10359a09c736f1f1076
eyao <2294553196@qq.com>
2019-03-01 13:21:34 +0800

    Java8 Stream API 映射

--- src/com/eyao/java8_01/TestStreamAPI2.java
+++ src/com/eyao/java8_01/TestStreamAPI2.java
@@ -2,6 +2,7 @@ package com.eyao.java8_01;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -31,6 +32,67 @@ public class TestStreamAPI2 {
     );
 
     // 中间操作
+
+    /*
+     映射
+        map——接收 Lambda ， 将元素转换成其他形式或提取信息。接收一个函数作为参数，该函数会被应用到每个元素上，并将其映射成一个新的元素。
+        flatMap——接收一个函数作为参数，将流中的每个值都换成另一个流，然后把所有流连接成一个流
+     */
+
+    @Test
+    public void test5() {
+        List<String> list = Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee");
+
+        list.stream()
+                .map((str) -> str.toUpperCase())
+                .forEach(System.out::println);
+
+        System.out.println("----------------------");
+
+        employees.stream()
+                .map(Employee::getName)
+                .forEach(System.out::println);
+
+        System.out.println("----------------------");
+
+//        Stream<Stream<Character>> stream = list.stream()
+//                .map(TestStreamAPI2::filterCharacter);// {{a,a,a},{b,b,b}}
+//
+//        stream.forEach((sm) -> sm.forEach(System.out::println));
+
+        System.out.println("----------------------");
+
+        Stream<Character> sm = list.stream()
+                .flatMap(TestStreamAPI2::filterCharacter); // {a,a,a,b,b,b}
+
+        sm.forEach(System.out::println);
+
+    }
+
+    @Test
+    public  void test6(){
+
+        List<String> list = Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee");
+
+        List list2 = new ArrayList();
+
+        list2.add(11);
+        list2.add(22);
+        list2.addAll(list);
+
+        System.out.println(list2);
+
+    }
+
+    public static Stream<Character> filterCharacter(String str) { // add(Object obj)  addAll(Collection coll)
+        List<Character> list = new ArrayList<>();
+
+        for (Character ch : str.toCharArray()) {
+            list.add(ch);
+        }
+        return list.stream();
+    }
+
     /*
         筛选与切片
         filter——接收 Lambda ， 从流中排除某些元素。
@@ -38,11 +100,6 @@ public class TestStreamAPI2 {
         skip(n) —— 跳过元素，返回一个扔掉了前 n 个元素的流。若流中元素不足 n 个，则返回一个空流。与 limit(n) 互补
         distinct——筛选，通过流所生成元素的 hashCode() 和 equals() 去除重复元素
      */
-
-    @Test
-    public void test5(){
-
-    }
 
     @Test
     public void test4() {
